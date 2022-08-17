d96f6470bc058abc4b7dc9d01370cf19a673652e
JamesKunstle <jkunstle@bu.edu>
2022-08-17 13:50:57 -0400

    env vars added

--- app.py
+++ app.py
@@ -6,18 +6,36 @@ import dash
 from dash import DiskcacheManager, CeleryManager, Input, Output, html
 
 using = None
-# locally, REDIS_URL=redis://127.0.0.1:6379
 
 if "REDIS_URL" in os.environ:
     # Use Redis & Celery if REDIS_URL set as an env variable
     from celery import Celery
 
-    redis_url = os.environ["REDIS_URL"][:8] +  ":" + os.getenv("database-password", "") + "@" + os.environ["REDIS_URL"][8:]
-
-    celery_app = Celery(__name__, broker=redis_url, backend=redis_url)
+    celery_app = Celery(__name__, broker=os.environ["REDIS_URL"], backend=os.environ["REDIS_URL"])
     background_callback_manager = CeleryManager(celery_app)
     print("using celery")
     using = "celery"
+
+elif "REDIS_SERVICE_HOST" in os.environ and "REDIS_SERVICE_PORT" in os.environ:
+
+    from celery import Celery
+
+    redis_host = os.getenv("REDIS_SERVICE_HOST", "127.0.0.1")
+    redis_pw = os.getenv("database-password", "")
+    redis_port = os.getenv("REDIS_SERVICE_PORT", "6379")
+
+
+    redis_string = f"redis://:{ redis_pw }@{ redis_host }:{ redis_port }"
+
+    celery_app = Celery(
+        __name__,
+        broker=redis_string,
+        backend=redis_string
+    )
+
+    background_callback_manager = CeleryManager(celery_app)
+    print("using celery w/ env vars")
+    using = "celery, env vars"
 
 else:
     # Diskcache for non-production apps when developing locally
